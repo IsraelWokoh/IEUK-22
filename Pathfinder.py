1ee1ca150fda08bff21adcffcf809d8727076432
Israel Wokoh <103059296+IsraelWokoh@users.noreply.github.com>
2022-07-04 19:28:49 +0100

    A working version! (though absurdly slow)
    Performs a depth-first search on the grid (essentially a graph, right?) - probably the reason it's so sluggish. When I attempted this, I didn't know any pathfinding algorithms like Dijkstra or A*.
    
    And it shows.

--- Pathfinder.py
+++ Pathfinder.py
@@ -1,8 +1,14 @@
-def createGrid(
+# Amazon Coding Exercise
+# Written by Israel Wokoh
+# Developed in Python 3.11.0b3, June 2022
+
+
+def createGrid( # Creates grid
         rows = 10,
         columns = 10
 ):
 
+    # Ran into weird assignment issues before, chose this method
     fullGrid = [[] for x in range(rows)]
     for x in range(rows * columns):
         fullGrid[x % rows].append([])
@@ -10,62 +16,71 @@ def createGrid(
     return fullGrid
 
 
-def display():
+def displayGrid(): # Displays grid
     for row in grid:
         print(row)
     print()
 
 
-def insertObstacles(locationsXY):
+def insertObstacles(locationsXY): # Inserts obstacles at predetermined locations
     for point in locationsXY:
         grid[point[1]][point[0]] = obstacle
 
 
-def withinGridBounds(next_position):
+def withinGridBounds(next_position): # Checks if out of bounds
     return next_position[y_index] in range(rows) \
     and next_position[x_index] in range(columns)
 
-def unobstructed(next_position):
+
+def unobstructed(next_position): # Checks if obstacle in the way
     return grid[next_position[y_index]][next_position[x_index]] is not obstacle
 
-def findPath(
+
+def findPath( # Depth-first search - EXTREMELY SLOW
         position=(0, 0),
-        path = [],
-        lastDirection = None,
+        path = [(0,0)],
+        step = 1,
         start=(0, 0),
         goal=(9, 9)
 ):
 
-    backtrack = list(moveChoices).index
+    for direction in moveChoices.keys(): # iterate over directions
 
-    for direction in [dir for dir in moveChoices.keys() if dir is not backtrack(lastDirection)]:
-        if position is not goal: # Goal not reached
-            if position is not move(position, direction): # if move is legal...
-                position = move(position, direction) # ...make that move
-                path.append(direction) # Add to current path
-                findPath(position, path, direction) # Recurse
-        else: # Goal reached
-            solutions.append(path) # Add full path to list of solutions
-            break
+        newPosition = move(position, direction)
 
-def backtrack(lastDirection): # Determines opposite direction
-    return opposite[lastDirection]
+        if newPosition not in path: # If position unvisited...
+            newPath = path + [newPosition]
+            if newPosition == goal: # if goal reached
+                if newPath not in solutions: # if NEW solution
+                    if solutions != []: # if NOT FIRST
+                        if len(newPath) < len(solutions[-1]): # if SHORTER
+                            solutions.remove(solutions[0]) # out with old...
+                            solutions.append(newPath) # ...in with new - clumsy for 1-element list. WILL CHANGE!
+                            print(f'{len(solutions[0])}-step solution found') # For testing
+                    else: # if first
+                        solutions.append(newPath) # append it
+                break # don't go any further down this path
 
-def move(position, direction):
-    next_position = [position[y_index + moveChoices[direction][y_index]],
-                     position[x_index + moveChoices[direction][x_index]]]
+            else: # if goal NOT reached
+                findPath(newPosition, newPath, step+1) # recurse
 
-    if withinGridBounds(next_position) and unobstructed(next_position): # If legal, do move
-        position = next_position
+
+def move(position, direction): # Changes position
+    next_position = (position[y_index] + moveChoices[direction][y_index],
+                     position[x_index] + moveChoices[direction][x_index]) # Calculate new position
+
+    if withinGridBounds(next_position) and unobstructed(next_position): # If legal...
+        position = next_position # do move
 
     return position
+
 
 if __name__ == '__main__':
     rows = 10
     columns = 10
     grid = createGrid(rows, columns)
 
-    # Allowed moves to reach goal
+    # Allowed moves: vector
     moveChoices = {
         'left': (0, -1),
         'up': (-1, 0),
@@ -77,20 +92,8 @@ if __name__ == '__main__':
         'right': (0, 1)
     }
 
-    # To avoid backtracking on the next move (unless necessary)
-    opposite = {
-        'left': 'right',
-        'up': 'down',
-        'down-right': 'up-left',
-        'down-left': 'up-right',
-        'up-right': 'down-left',
-        'up-left': 'down-right',
-        'down': 'up',
-        'right': 'left',
-        None: None
-    }
-
     # Preset obstacle locations
+    obstacle = '✖' # Obstacle icon
     locationsXY = (
         (9, 7),
         (8, 7),
@@ -98,8 +101,7 @@ if __name__ == '__main__':
         (6, 8)
     )
 
-    obstacle = '✖'
-    insertObstacles(locationsXY)
+    insertObstacles(locationsXY) # Places obstacles
 
     playerIcon = '▣'
 
@@ -107,16 +109,14 @@ if __name__ == '__main__':
     grid[0][0] = '○'
     grid[9][9] = '☆'
 
-
-
     # NOTE: [row][column] means [y][x]
     y_index = 0
     x_index = 1
 
-    solutions = []
-    findPath()
-    if len(solutions) > 0:
+    solutions = [] # List of solutions - at most one element (will change)
+    findPath() # Calls pathfinding function
+
+    if len(solutions) != 0: # Never make it here because I'm too slow.
         print(solutions)
     else:
         print('No paths found.')
-
